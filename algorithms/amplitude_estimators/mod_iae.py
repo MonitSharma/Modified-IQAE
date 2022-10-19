8d6782b83c440db6c610c44a30ed3806b75e6e76
Shion Fukuzawa <fukuzaws@circinus-34.ics.uci.edu>
2022-10-19 11:29:15 -0700

    Added ChebAE heuristic to improve numerical performance

--- algorithms/amplitude_estimators/mod_iae.py
+++ algorithms/amplitude_estimators/mod_iae.py
@@ -343,14 +343,38 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
                 round_shots = 0
                 
                 while powers[num_iterations - 1] == k:
-                    
-                    N = min(round_shots + shots, shots_i_max)
 
-                    shots = N - round_shots
-                    if self._quantum_instance:
-                        self._quantum_instance._run_config.shots = N - round_shots
-                    
-                    round_shots = N
+                    # Early/late condition by ChebAE
+                    if self._confint_method == "beta":
+                        nu = 8
+                        # If early...
+                        if self._check_early(shots, alpha_i, nu, theta_intervals, self._epsilon):
+                            print(f"Early: k = {k}")
+                            N = min(round_shots + shots, shots_i_max)
+
+                            shots = N - round_shots
+                            if self._quantum_instance:
+                                self._quantum_instance._run_config.shots = N - round_shots
+                            
+                            round_shots = N
+                        # If late...
+                        else:
+                            print(f"Late: k = {k}")
+                            if self._quantum_instance:
+                                self._quantum_instance._run_config.shots = 1
+
+                            N = min(round_shots + 1, shots_i_max)                            
+                            round_shots = N
+
+                    # Standard Chernoff version                         
+                    else: 
+                        N = min(round_shots + shots, shots_i_max)
+
+                        shots = N - round_shots
+                        if self._quantum_instance:
+                            self._quantum_instance._run_config.shots = N - round_shots
+                        
+                        round_shots = N
 
                     if verbose:
                         print()
@@ -505,6 +529,22 @@ class ModifiedIterativeAmplitudeEstimation(AmplitudeEstimator):
         state['ks'] = powers
 
         return result
+
+    def _check_early(shots, alpha, nu, theta_intervals, eps):
+        import math 
+        # Get max eps threshold 
+        eps_max = -math.inf 
+        for x in range(1, shots):
+            lower = beta.ppf(1 - alpha / 2, x + 1, n - x)
+            upper = beta.ppf(alpha / 2, x, n - x + 1)
+            if math.abs(upper - lower) > eps_max:
+                eps_max = math.abs(upper - lower)
+
+        num = theta_intervals[-1][1] - theta_intervals[-1][0]
+        den = math.abs(math.sin(theta_intervals[-1][1] - math.sin[-1][0]))
+
+        return eps_max * num / den <= eps * nu 
+        
 
 
 class ModifiedIterativeAmplitudeEstimationResult(AmplitudeEstimatorResult):
